0092186624d08b97070126f818afe0dd0208e6bc
Thiago Oliveira <thiago@outlook.com.br>
2026-04-18 14:42:52 -0300

    Add expense use case protocols

---
+++ AISmartExpenseAnalyzer/AISmartExpenseAnalyzer/Domain/UseCases/Protocols/DeleteExpenseUseCaseProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  DeleteExpenseUseCaseProtocol.swift
+//  AISmartExpenseAnalyzer
+//
+//  Created by Thiago Oliveira on 18/04/26.
+//
+
+import Foundation
+
+protocol DeleteExpenseUseCaseProtocol {
+    /// Remove a despesa com o id fornecido do armazenamento
+    /// - Throws: `RepositoryError.notFound` se nenhuma despesa correspondente existir
+    func execute(id: UUID) async throws
+}
