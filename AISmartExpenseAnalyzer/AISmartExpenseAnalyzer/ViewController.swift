f980d5ec5bd48bad8c97a71c5624eaa83a026f6f
Thiago Oliveira <thiago@outlook.com.br>
2026-04-18 11:38:27 -0300

    Add initial iOS app project scaffold

---
+++ AISmartExpenseAnalyzer/AISmartExpenseAnalyzer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AISmartExpenseAnalyzer
+//
+//  Created by Thiago Oliveira on 18/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
